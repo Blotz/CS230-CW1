e301e33722f6b297b97ce40ef9fe5057fb73ae54
Blotz <f.p.theil@gmail.com>
2022-11-25 11:24:19 +0000

    Build 2d array of Tiles

--- src/main/java/com/group10/Tile.java
+++ src/main/java/com/group10/Tile.java
@@ -1,12 +1,16 @@
 package com.group10;
 
 public class Tile {
-    private String[] tiles = new String[4] ;
-    public Tile (String c1, String c2, String c3, String c4) {
+    private char[] tiles = new char[4] ;
+    public Tile (char c1, char c2, char c3, char c4) {
         tiles[0] = c1;
         tiles[1] = c2;
         tiles[2] = c3;
         tiles[3] = c4;
     }
     
+    @Override
+    public String toString() {
+        return String.valueOf(tiles[0]) + String.valueOf(tiles[1]) + String.valueOf(tiles[2]) + String.valueOf(tiles[3]);
+    }
 }
--- src/main/test/com/group10/TileTest.java
+++ src/main/test/com/group10/TileTest.java
@@ -8,7 +8,7 @@ class TileTest {
     
     @Test
     void Tile() {
-        Tile tile = new Tile("r","g","b","c");
+        Tile tile = new Tile('r','g','b','c');
     }
     
 
