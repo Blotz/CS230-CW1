02d2c9bd196929f9466b65ca4a25ff11ef5bbab9
Blotz <f.p.theil@gmail.com>
2022-11-25 10:25:01 +0000

    Added basic constructor for Tile

---
+++ src/main/test/com/group10/TileTest.java
@@ -0,0 +1,15 @@
+package com.group10;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileTest {
+    
+    @Test
+    void Tile() {
+        Tile tile = new Tile("r","g","b","c");
+    }
+    
+
+}
