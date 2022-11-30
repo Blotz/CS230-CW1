75ea75d7bd00e79ca0f156aab02354caac1cc95b
lewishendy <118919858+lewishendy@users.noreply.github.com>
2022-11-30 15:42:10 +0000

    Updated Menu

--- src/main/java/com/group10/Main.java
+++ src/main/java/com/group10/Main.java
@@ -6,6 +6,8 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -19,10 +21,16 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -42,94 +50,113 @@ import java.nio.file.Paths;
  * @author Liam O'Reilly
  */
 public class Main extends Application {
-	// The dimensions of the window
-	private static final int WINDOW_WIDTH = 800;
-	private static final int WINDOW_HEIGHT = 500;
+	private Parent createContent() {
+		Pane root = new Pane();
 
-	// The dimensions of the canvas
-	private static final int CANVAS_WIDTH = 600;
-	private static final int CANVAS_HEIGHT = 400;
-
-	// The width and height (in pixels) of each cell that makes up the game.
-	private static final int GRID_CELL_WIDTH = 50;
-	private static final int GRID_CELL_HEIGHT = 50;
-	
-	// The width of the grid in number of cells.
-	private static final int GRID_WIDTH = 12;
-	
-	// The canvas in the GUI. This needs to be a global variable
-	// (in this setup) as we need to access it in different methods.
-	// We could use FXML to place code in the controller instead.
-	private Canvas canvas;
-		
-	// Loaded images
-	private Image playerImage;
-	private Image dirtImage;
-	private Image iconImage;
-	
-	// X and Y coordinate of player on the grid.
-	private int playerX = 0;
-	private int playerY = 0;
-	
-	// Timeline which will cause tick method to be called periodically.
-	private Timeline tickTimeline; 
-	
-	/**
-	 * Setup the new application.
-	 * @param primaryStage The stage that is to be used for the application.
-	 */
-	public void start(Stage primaryStage) {
-		// Load images. Note we use png images with a transparent background.
-		String url = Main.class.getResource("player.png").toString();
-		playerImage = new Image(url);
-		url = String.valueOf(Main.class.getResource("dirt.png"));
-		dirtImage = new Image(url);
-		url = String.valueOf(Main.class.getResource("icon.png"));
-		iconImage = new Image(url);
-
-
-		// Build the GUI 
-		Pane root = buildGUI();
-		
-		// Create a scene from the GUI
-		Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-
-	private static Pane buildGUI() {
-		// Create top-level panel that will hold all GUI nodes.
-		BorderPane root = new BorderPane();
-		// Create a toolbar with some nice padding and spacing
-		HBox toolbar = new HBox();
-		toolbar.setSpacing(10);
-		toolbar.setPadding(new Insets(10, 10, 10, 10));
-		root.setTop(toolbar);
+		root.setPrefSize(1050, 600);
 
 		try(InputStream is = Files.newInputStream(Paths.get("src/main/resources/com/group10/BACKROUND.jpg"))){
 			ImageView img = new ImageView(new Image(is));
 			img.setFitWidth(1050);
 			img.setFitHeight(600);
-			//root.getChildren().add(img);
+			root.getChildren().add(img);
 		}
 		catch(IOException e) {
 			System.out.println("Couldn't load image");
 		}
 
-		//Add a input feature to display motd and leaderboard
-		Label title = new Label("Menu");
-		Label motd = new Label("Message of the day");
-		Label leaderboard = new Label("LeaderBoard");
+		Title title = new Title ("Cave game");
+		title.setTranslateX(50);
+		title.setTranslateY(200);
 
-		Button game = new Button("Go to Level");
-		game.setOnAction(e -> Game.display());
+		MenuBox vbox = new MenuBox(new MenuItem("Level"));
+		vbox.setTranslateX(100);
+		vbox.setTranslateY(300);
 
-		// We add both buttons at the same time to the timeline (we could have done this in two steps).
-		toolbar.getChildren().addAll(title, game, motd, leaderboard);
+		root.getChildren().addAll(title,vbox);
 
-		// Finally, return the border pane we built up.
 		return root;
+
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Scene scene = new Scene(createContent());
+		primaryStage.setTitle("Cave Game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	private static class Title extends StackPane {
+		public Title(String name) {
+			Rectangle bg = new Rectangle(375, 60);
+			bg.setStroke(Color.WHITE);
+			bg.setStrokeWidth(2);
+			bg.setFill(null);
+
+			Text text = new Text(name);
+			text.setFill(Color.WHITE);
+			text.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD, 50));
+
+			setAlignment(Pos.CENTER);
+			getChildren().addAll(bg,text);
+		}
+	}
+
+	private static class MenuBox extends VBox {
+		public MenuBox(MenuItem...items) {
+			getChildren().add(createSeperator());
+
+			for(MenuItem item : items) {
+				getChildren().addAll(item, createSeperator());
+			}
+		}
+
+		private Line createSeperator() {
+			Line sep = new Line();
+			sep.setEndX(210);
+			sep.setStroke(Color.DARKGREY);
+			return sep;
+		}
+
+	}
+
+
+	private static class MenuItem extends StackPane{
+		public MenuItem(String name) {
+			LinearGradient gradient = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[] {
+					new Stop(0, Color.DARKBLUE),
+					new Stop(0.1, Color.BLACK),
+					new Stop(0.9, Color.BLACK),
+					new Stop(1, Color.DARKBLUE)
+
+			});
+
+			Rectangle bg = new Rectangle(200,30);
+			bg.setOpacity(0.4);
+
+			Text text = new Text(name);
+			text.setFill(Color.DARKGREY);
+			text.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD,20));
+
+			setAlignment(Pos.CENTER);
+			getChildren().addAll(bg, text);
+			setOnMouseEntered(event -> {
+				bg.setFill(gradient);
+				text.setFill(Color.WHITE);
+
+			});
+
+			setOnMouseExited(event -> {
+				bg.setFill(Color.BLACK);
+				text.setFill(Color.DARKGREY);
+			});
+			setOnMousePressed(event -> Game.display());
+
+			setOnMouseReleased(event -> {
+				bg.setFill(gradient);
+			});
+
+		}
 	}
 
 	public static void main(String[] args) {
