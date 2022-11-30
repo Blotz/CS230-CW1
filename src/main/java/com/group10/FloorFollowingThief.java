ef37b9cb7c9a5a2222766b7fbaad1587c10cbe91
alexm-p <alexmakpearce01@gmail.com>
2022-11-30 17:01:36 +0000

    Updated move for Flying Assassin
    also added direction Enums for moveable entities

--- src/main/java/com/group10/FloorFollowingThief.java
+++ src/main/java/com/group10/FloorFollowingThief.java
@@ -10,4 +10,25 @@ public class FloorFollowingThief extends MoveableEntity {
         this.y = y;
         this.colour = colour;
     }
+
+    @Override
+    public int[] moveRight(Level level, int x, int y) {
+        return new int[]{x + 1, y};
+    }
+
+    @Override
+    public int[] moveLeft(Level level, int x, int y) {
+        return new int[]{x - 1, y};
+    }
+
+    @Override
+    public int[] moveDown(Level level, int x, int y) {
+        return new int[]{x, y - 1};
+    }
+
+    @Override
+    public int[] moveUp(Level level, int x, int y) {
+        return new int[]{x, y + 1};
+    }
+
 }
--- src/main/java/com/group10/FlyingAssassin.java
+++ src/main/java/com/group10/FlyingAssassin.java
@@ -5,10 +5,58 @@ import java.util.Objects;
 public class FlyingAssassin extends MoveableEntity{
     private int x;
     private int y;
+    private Direction direction;
 
-    public FlyingAssassin(int x, int y) {
+    public FlyingAssassin(int x, int y, Direction direction) {
         this.x = x;
         this.y = y;
+        this.direction = direction;
+    }
+
+    public int[] move(Level level) {
+        if (direction == Direction.UP || direction == Direction.DOWN) {
+            return moveVertical(level);
+        } else {
+            return moveHorizontal(level);
+        }
+    }
+
+    public int[] moveHorizontal(Level level) {
+        if (direction == Direction.RIGHT) {
+            if (x < level.MAX_WIDTH) {
+                return moveRight(level, x, y);
+            } else {
+                direction = Direction.LEFT;
+                return moveLeft(level, x, y);
+            }
+        } else if (direction == Direction.LEFT) {
+            if (x > 0) {
+                return moveLeft(level, x, y);
+            } else {
+                direction = Direction.RIGHT;
+                return moveRight(level, x, y);
+            }
+        }
+        return new int[]{x, y};
+    }
+
+    public int[] moveVertical(Level level) {
+        if (direction == Direction.UP) {
+            if (y < level.MAX_HEIGHT) {
+                return moveUp(level, x, y);
+            } else {
+                direction = Direction.DOWN;
+                return moveDown(level, x, y);
+            }
+        } else if (direction == Direction.DOWN) {
+            if (y > 0) {
+                return moveDown(level, x, y);
+            } else {
+                direction = Direction.UP;
+                return moveUp(level, x, y);
+            }
+        }
+        return new int[]{x, y};
     }
 
     @Override
@@ -33,8 +81,15 @@ public class FlyingAssassin extends MoveableEntity{
 
     @Override
     public boolean equals(Object o) {
-        //NEED TO DO
-        return true;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlyingAssassin that = (FlyingAssassin) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
 
