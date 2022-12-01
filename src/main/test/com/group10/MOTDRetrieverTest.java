308a6d0c1605d0c5657320d899b8ba502ebf9c0d
Blotz <f.p.theil@gmail.com>
2022-12-01 11:37:27 +0000

    Added test for MODT

---
+++ src/main/test/com/group10/MOTDRetrieverTest.java
@@ -0,0 +1,13 @@
+import com.group10.MOTDRetriever;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MOTDRetrieverTest {
+    
+    @Test
+    void getMOTD() {
+        System.out.println(MOTDRetriever.getMOTD());
+    }
+}
