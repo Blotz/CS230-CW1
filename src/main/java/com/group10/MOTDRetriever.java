c897d63c1428bc4add9846a46a20fd1bca2fb060
Luca <luca.collicott.01@protonmail.ch>
2022-12-01 00:02:07 +0000

    Further progress + optimised code
    i'm so good at coding

--- src/main/java/com/group10/MOTDRetriever.java
+++ src/main/java/com/group10/MOTDRetriever.java
@@ -5,35 +5,68 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.*;
 
-    /*
+    /**
+     * @author Luca
+     * @version 1.0 Class made to collect MOTD via HTTP GET Request
      * MOTDGetter handles creating the GET request to pull the MOTD from
      * cswebcat.swansea.ac.uk, it also features methods to handle exceptions
      * and solve the validification puzzle to access the MOTD.
      */
 public class MOTDRetriever {
-    private Character[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y', 'z' };
+    private static String alphabet = "abcdefghijklmnopqrstuvwyxz";
 
-    // MOTDGetter creates an instance of HttpRequest to create a GET Request
-    // and return the results of the GET Request
-    private static String MOTDGetter() 
-        throws URISyntaxException, IOException, InterruptedException{
-        HttpRequest getMOTD = HttpRequest.newBuilder()
-        .uri(new URI("http://cswebcat.swansea.ac.uk/puzzle"))
+    /**
+     * MOTDGetter sends a GET request and returns the body of the response
+     * @param url URL to send the GET request too
+     * @return Returns the body of the packet sent back by the server
+     * @throws Exception URISyntaxException, IOException, InterruptedException
+     */
+    private static String MOTDGetter(String url) 
+        throws Exception{
+        HttpRequest getResponse = HttpRequest.newBuilder()
+        .uri(new URI(url))
         .GET()
         .build();
 
         HttpResponse<String> response = HttpClient.newHttpClient()
-            .send(getMOTD, HttpResponse.BodyHandlers.ofString());
-        HttpHeaders responseHeaders = response.headers();
-        return responseHeaders.toString();
+            .send(getResponse, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
     }
 
-    // Wrapper to catch URISyntaxException thrown by MOTDRetriever()
+    /**
+     * @param puzzle current auth puzzle sent by cswebcat
+     * @return solution to the cracked ciphertext.
+     */
+    private static String Solve(String puzzle) {
+        String password = "";
+        int newIndex;
+        for (int i = 0; i < puzzle.length(); i++){
+            newIndex = alphabet.indexOf(puzzle.substring(i));
+            if (i % 2 == 0){
+                newIndex = newIndex + i > 25 
+                    ? (newIndex + i - 25) : newIndex + i;
+                password += alphabet.charAt(newIndex);
+            } else {
+                newIndex = newIndex - i < 0
+                    ? (newIndex + i + 25) : newIndex + i;
+                password += alphabet.charAt(newIndex - i);
+            }
+        }
+        password = password.length() + password;
+        password += "CS-230";
+        return password;
+    }
+
+    /**
+     * getMOTD is a wrapper to catch exceptions from MOTDGetter and
+     * handles sending both requests and returning todays MOTD
+     * @return The current MOTD from cswebcat.swansea.ac.uk/message
+     */
     public static String getMOTD() {
         try {
-            return MOTDGetter();
+            String keyword = Solve(MOTDGetter("http://cswebcat.swansea.ac.uk/puzzle"));
+            return MOTDGetter("http://cswebcat.swansea.ac.uk/message?solution=" + keyword);
         } catch (Exception e) {
             System.out.print(e);
             return "Connection to cswebcat.swansea.ac.uk/puzzle failed.";
