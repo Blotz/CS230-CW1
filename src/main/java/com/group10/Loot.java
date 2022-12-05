1dc0181cd8d59cea56abd61f954c1611f2f9a75f
Blotz <f.p.theil@gmail.com>
2022-12-05 18:59:41 +0000

    Added value to Loot constructor

--- src/main/java/com/group10/Loot.java
+++ src/main/java/com/group10/Loot.java
@@ -4,6 +4,9 @@ public class Loot extends PickUp {
     int value;
     int xpos;
     int ypos;
+    public Loot(int value) {
+        this.value = value;
+    }
     
     @Override
     public void onInteract(Player player) {
