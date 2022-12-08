3a357dd3ce01a960e6237df93da9ca4c6c1523b7
alexm-p <alexmakpearce01@gmail.com>
2022-12-08 13:24:25 +0000

    Made FFT not move out of the game hopfully

--- src/main/java/com/group10/FloorFollowingThief.java
+++ src/main/java/com/group10/FloorFollowingThief.java
@@ -14,24 +14,36 @@ public class FloorFollowingThief extends MoveableEntity {
     }
 
     public int[] move(Level level) {
+        int[] newPos;
         if (direction == Direction.RIGHT) {
-            return moveRight(level, x, y);
+            newPos = moveRight(level, x, y);
+            //Needs check for if end has been reached
+            return newPos;
         } else if (direction == Direction.LEFT) {
-            return moveLeft(level, x ,y);
+            newPos = moveLeft(level, x, y);
+            return newPos;
         } else if (direction == Direction.UP) {
-            return moveUp(level, x, y);
+            newPos = moveUp(level, x, y);
+            return newPos;
         } else if (direction == Direction.DOWN) {
-            return moveDown(level, x, y);
+            newPos = moveDown(level, x, y);
+            return newPos;
         }
         return new int[]{x, y};
     }
 
+    public boolean leftHandTouching() {
+        return true; //Placeholder method to make sure the left hand of FFT is touching a wall with left hand.
+    }
+
     @Override
     public int[] moveRight(Level level, int x, int y) {
-        char[] colorsOnNewTile = level.getTileColor(x + 1, y);
-        for (char newColor : colorsOnNewTile) {
-            if (newColor == colour) {
-                return new int[]{x + 1, y};
+        if (x + 1 <= level.MAX_WIDTH) {
+            char[] colorsOnNewTile = level.getTileColor(x + 1, y);
+            for (char newColor : colorsOnNewTile) {
+                if (newColor == colour) {
+                    return new int[]{x + 1, y};
+                }
             }
         }
         return new int[]{x , y};
@@ -39,10 +51,12 @@ public class FloorFollowingThief extends MoveableEntity {
 
     @Override
     public int[] moveLeft(Level level, int x, int y) {
-        char[] colorsOnNewTile = level.getTileColor(x - 1, y);
-        for (char newColor : colorsOnNewTile) {
-            if (newColor == colour) {
-                return new int[]{x - 1, y};
+        if (x - 1 >= 0) {
+            char[] colorsOnNewTile = level.getTileColor(x - 1, y);
+            for (char newColor : colorsOnNewTile) {
+                if (newColor == colour) {
+                    return new int[]{x - 1, y};
+                }
             }
         }
         return new int[]{x , y};
@@ -50,10 +64,12 @@ public class FloorFollowingThief extends MoveableEntity {
 
     @Override
     public int[] moveDown(Level level, int x, int y) {
-        char[] colorsOnNewTile = level.getTileColor(x, y - 1);
-        for (char newColor : colorsOnNewTile) {
-            if (newColor == colour) {
-                return new int[]{x, y - 1};
+        if (y - 1 >= 0) {
+            char[] colorsOnNewTile = level.getTileColor(x, y - 1);
+            for (char newColor : colorsOnNewTile) {
+                if (newColor == colour) {
+                    return new int[]{x, y - 1};
+                }
             }
         }
         return new int[]{x , y};
@@ -61,10 +77,12 @@ public class FloorFollowingThief extends MoveableEntity {
 
     @Override
     public int[] moveUp(Level level, int x, int y) {
-        char[] colorsOnNewTile = level.getTileColor(x, y + 1);
-        for (char newColor : colorsOnNewTile) {
-            if (newColor == colour) {
-                return new int[]{x, y + 1};
+        if (y + 1 <= level.MAX_HEIGHT) {
+            char[] colorsOnNewTile = level.getTileColor(x, y + 1);
+            for (char newColor : colorsOnNewTile) {
+                if (newColor == colour) {
+                    return new int[]{x, y + 1};
+                }
             }
         }
         return new int[]{x , y};
