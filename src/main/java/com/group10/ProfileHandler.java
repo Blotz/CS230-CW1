ac56f2ea75f3acfc55df5e90402ac64ef2d1fc68
alexm-p <alexmakpearce01@gmail.com>
2022-12-09 11:59:01 +0000

    Updated profile handler to select and create profiles

--- src/main/java/com/group10/ProfileHandler.java
+++ src/main/java/com/group10/ProfileHandler.java
@@ -1,7 +1,6 @@
 package com.group10;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -12,13 +11,15 @@ different class but I need somewhere to put the code for now.
  */
 public class ProfileHandler {
     private ArrayList<Profile> profiles;
+    private static String PROFILES_PATH;
 
     private static final String FILE_NOT_FOUND = " didn't resolve to a file";
     private static final String INCORRECT_NAME_AND_LEVEL = " Incorrect file format, should be in: profileName|maxLevel";
 
     public ProfileHandler(String profilesPath) throws FileNotFoundException {
+        PROFILES_PATH = profilesPath;
         // Load file from resources path
-        InputStream file = Level.class.getResourceAsStream(profilesPath);
+        InputStream file = ProfileHandler.class.getResourceAsStream(profilesPath);
         // If the path is invalid, throw an error!
         if (file == null) {
             throw new FileNotFoundException(profilesPath + FILE_NOT_FOUND);
@@ -38,12 +39,27 @@ public class ProfileHandler {
         }
     }
 
-    public ArrayList<Profile> getProfiles() {
-        return profiles;
+    public ArrayList<Profile> selectProfiles() {
+        if (profiles == null) {
+            System.out.println("No profiles have been created!");
+            return null;
+        } else {
+            return profiles;
+        }
     }
 
-    public void setProfiles(ArrayList<Profile> profiles) {
-        this.profiles = profiles;
-        //Code needed to add the new profile to the .txt file.
+    public Profile createProfile(String playerName) {
+        Profile newProfile = new Profile(playerName, 1);
+        profiles.add(newProfile);
+        //Code to write the new profile to profiles.txt
+        try {
+            FileWriter myWriter = new FileWriter(PROFILES_PATH);
+            myWriter.write(playerName + "|" + "1");
+            myWriter.close();
+            System.out.println("New profile created!");
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+        }
+        return newProfile;
     }
 }
